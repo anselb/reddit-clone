d793ff4ad451a4a1bbf1cc633179960b7daf7960
Ansel Bridgewater <iamansel@gmail.com>
2017-09-26 10:30:51 -0700

    Wrote tests that don't have full functionality yet

--- test/posts.js
+++ test/posts.js
@@ -3,22 +3,33 @@ var chaiHttp = require('chai-http')
 var should = chai.should()
 var Post = require('../models/post')
 
+var agent = chai.request.agent(server)
+
 chai.use(chaiHttp)
 
-var tour = {title: 'post title', url: 'https://www.google.com', summary: 'post summary', subreddit: 'post subreddit'}
+before(function (done) {
+    agent
+        .post('/login')
+        .send({ username: 'testone', password: 'password'})
+        .end(function (err, res) {
+            done()
+        })
+})
+
+var post = {title: 'post title', url: 'https://www.google.com', summary: 'post summary', subreddit: 'post subreddit'}
 
 describe('Posts', function() {
     it('should create with valid attributes at POST /photos', function(done) {
-        Post.findOneAndRemove(tour, function() {
+        Post.findOneAndRemove(post, function() {
             //how many tours are there now?
-            Post.find(function(err, tours) {
-                var tourCount = tours.length
+            Post.find(function(err, posts) {
+                var postCount = posts.length
                 chai.request('localhost:3000')
-                .post('/posts', tour)
+                .post('/posts', post)
                 .end(function (err, res) {
                     //check database for one more tour
-                    Post.find(function(err, tours) {
-                        tourCount.should.be.equal(tours.length)
+                    Post.find(function(err, posts) {
+                        postCount.should.be.equal(posts.length)
 
                         //check response is successful
                         res.should.have.status(200)
